9b47276074dbe5a33bf804e9dc2df65b4f3d4ce0
EdgarM99 <36931461+EdgarM99@users.noreply.github.com>
2018-03-09 10:23:52 -0500

    Add files via upload

---
+++ parcialju/src/parcialju/calculos.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package parcialju;
+
+/**
+ *
+ * @author hnos Salgado
+ */
+public class calculos {
+
+    
+    
+  
+    
+    
+    
+    
+    public static void suma() {
+        
+    
+    
+    int r=0;
+int p = 0;
+int pe = 0;
+int cal= r+p+pe;
+int pro= cal/3;
+
+
+if(r>65){
+            System.out.println("Esta en condiciones para jugar " + r + " minutos o mas ");
+      
+      if(pro < 65){
+           System.out.println("Tiene buena resistencia pero mal global solo puede jugar 40 minutos o menos");
+       
+       }
+       }            
+      else{
+            System.out.println("Esta en condiciones para jugar " + r + " minutos o menos ");
+          
+          }
+if (pro > 65  ){
+                      
+      
+      System.out.println("Tiene un global :" + pro);
+           
+   
+   
+       
+   
+   
+   }
+   else{
+   
+   
+            System.out.println("Tiene un global :" + pro);
+       
+   
+   
+   } 
+       
+ if (pro > 65 ){
+                      
+      
+                        System.out.println("Esta bien acondicionado fisicamente   ");
+   
+   
+       
+   
+   
+   }
+   else{
+   
+   
+            
+            System.out.println("Esta mal acondicionado fisicamente  " );
+   
+   
+   } 
+       
+ 
+       
+       
+       
+       
+       
+   
+   
+   
+   }
+   
+}
+
+  
+
